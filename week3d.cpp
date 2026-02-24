27b1e7abe3e6e0ceda91ec59ea4f1d3bd6cfe230
Ragola Arun <ragolaarun2007@gmail.com>
2026-02-24 20:03:43 +0530

    Add files via upload

---
+++ week3d.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include  <set>
+#include <algorithm>
+using namespace std;
+int main () {
+    set<int> s;
+s.insert(30);
+s.insert(10);
+s.insert(20);
+s.insert(10);
+cout <<"set elements(sorted &unique):";
+for (set<int>::iterator it =s.begin();it!=s.end();it++)
+cout<<*it<<" ";
+int key=20;
+if(s.find(key)!=s.end())
+cout<<"\nElement"<<key<<"found";
+else
+cout<<"\n Element not found";
+return 0;
+}
